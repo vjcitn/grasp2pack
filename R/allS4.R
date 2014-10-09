6ddca3aaa78586899af652ee25308787c5636dc1
Vince Carey <stvjc@channing.harvard.edu>
2014-10-09 15:50:10 -0400

    vignette expanded to include gr2apply

--- R/allS4.R
+++ R/allS4.R
@@ -34,3 +34,24 @@ setMethod("show", "g2GRanges", function(object) {
   show(as(object[, cols2show], "GRanges"))
   })
 
+setGeneric("loadResults", 
+  function (reg, ids, part = NA_character_, simplify = FALSE, use.names = "ids", missing.ok = FALSE) 
+    standardGeneric("loadResults"))
+
+setMethod("loadResults", c("g2reg", "ANY", "character", "logical",
+   "character", "logical"),
+  function (reg, ids, part = NA_character_, simplify=FALSE, use.names="ids", missing.ok = FALSE) {
+  ans = BatchJobs::loadResults(reg, ids, part, simplify, use.names, missing.ok)
+  ans = GenomicRanges::unlist(GRangesList(ans))
+  seqlevelsStyle(ans) = "UCSC"
+  sn = seqlevels(ans)
+  seqinfo(ans) = hg19si()[sn]
+  new("g2GRanges", ans)
+  })
+
+setMethod("loadResults", c("g2reg", "ANY", "missing", "missing", "missing",
+    "missing"),
+   function (reg, ids, 
+     part = NA_character_, simplify=FALSE, use.names="ids", missing.ok = FALSE) {
+  loadResults(reg, ids, NA_character_, FALSE, "ids", FALSE)
+  })
