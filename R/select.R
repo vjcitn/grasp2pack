13f3263f51c65a16f36b92dc22c3699067343dc4
Vince Carey <stvjc@channing.harvard.edu>
2014-10-12 12:03:22 -0400

    add a selector for GRanges

--- R/select.R
+++ R/select.R
@@ -8,3 +8,22 @@ selectByPapPheDescs = function(desc) {
   tmp = bplapply(tmp, function(x) as(x[ which(x$PaperPhenotypeDescription %in% desc) ], "GRanges"))
   new("g2GRanges", GenomicRanges::unlist(GRangesList(tmp)))
 }
+
+selectByGRanges = function (gr)
+{
+    if (!exists("grasp2index")) 
+        data(grasp2index)
+    inds = which(grasp2index %over% gr)
+    if (length(inds) == 0) 
+        stop("gr not overlapped by any index element")
+    inds = (grasp2index[inds])$jobid
+    if (length(inds)==1) {
+      tmp = loadResult(GRASP2, inds)
+      tmp = tmp[ which(tmp %over% gr) ]
+      return(tmp)
+      }
+    tmp = loadResults(GRASP2, inds)
+    tmp = bplapply(tmp, function(x) x[ which(x %over% gr) ] )
+    new("g2GRanges", GenomicRanges::unlist(GRangesList(tmp)))
+}
+
